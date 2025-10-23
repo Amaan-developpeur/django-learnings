4ee95ed928e8839aed45b486db9c04ad58e7ec86
Amaan-developpeur <mohammedamaanb@gmail.com>
2025-10-23 15:26:46 +0530

    Removed one hello world Folder

--- django-practise/mon_project_premier/application3/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.display),
-    path("welcome/", views.welcome),
-    path("pagehtml/", views.template_function)
-]
-# "" means this is the default page of application3
