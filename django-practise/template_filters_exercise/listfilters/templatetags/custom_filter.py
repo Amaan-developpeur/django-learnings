8e7231ca20be35513a709308161b3ee0197a2fba
Amaan-developpeur <mohammedamaanb@gmail.com>
2025-10-24 19:43:44 +0530

    [Templates] Add custom filters to template filters (list filters module)

---
+++ django-practise/template_filters_exercise/listfilters/templatetags/custom_filter.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def capitalize_first(value):
+    return value.capitalize()
--- django-practise/template_filters_exercise/template_filters_exercise/settings.py
+++ django-practise/template_filters_exercise/template_filters_exercise/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "syntax_intro",
     "textfilters",
     "numericfilters",
+    "listfilters",
 ]
 
 MIDDLEWARE = [
